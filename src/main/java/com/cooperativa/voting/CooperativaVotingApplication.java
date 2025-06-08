4b1abbd36104696cb5416374b6c1cbf6c6d43491
Jonathan Santos <jonathan.sousa.df@gmail.com>
2025-06-08 14:58:41 -0300

    first commit

---
+++ src/main/java/com/cooperativa/voting/CooperativaVotingApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativa.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativaVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativaVotingApplication.class, args);
+	}
+
+}

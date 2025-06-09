aeec0feec234c29572b7ec54ef221717a4600ea8
Jonathan Santos <jonathan.sousa.df@gmail.com>
2025-06-08 22:34:51 -0300

    refactory

---
+++ src/test/java/com/cooperativa/voting/cooperativa_voting/CooperativaVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativa.voting.cooperativa_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativaVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
